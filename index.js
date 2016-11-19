be37fa3679da817e04bddb6c263abcfd8e1329ad
Bogdan Paliy <bpalij@gmail.com>
2016-11-19 18:57:48 +0200

    First commit. The number of examples in portfolio will increasein the future

---
+++ index.js
@@ -0,0 +1,27 @@
+// $(document).ready(function(){
+// 	alert("js ready");
+// });
+$('#portfolio1').click(function () {
+	document.location.href="portfolio/1.html"
+	// alert("true");
+});
+$('.btn-social').mouseenter(function(){
+	$(this).addClass("btn-primary");
+	$(this).removeClass("btn-default");
+})
+$('.btn-social').mouseleave(function(){
+	$(this).removeClass("btn-primary");
+	$(this).addClass("btn-default");
+})
+$('#btn-linkedin').click(function (){
+	document.location.href="https://ua.linkedin.com/in/bpalij"
+})
+$('#btn-github').click(function (){
+	document.location.href="https://github.com/bpalij"
+})
+$('#btn-codepen').click(function (){
+	document.location.href="https://codepen.io/bpalij/"
+})
+$('#btn-fcc').click(function (){
+	document.location.href="https://www.freecodecamp.com/bpalij"
+})
