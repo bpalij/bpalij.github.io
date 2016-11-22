a6dad19c2284c9a11d548a647bdfa2027ddb7eaf
Bogdan Paliy <bpalij@gmail.com>
2016-11-22 12:55:47 +0200

    Change jQuery click event to native JS onclick

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // $(document).ready(function(){
 // 	alert("js ready");
 // });
-$('#portfolio1').click(function () {
-	document.location.href="portfolio/1.html"
-	// alert("true");
-});
+// $('#portfolio1').click(function () {
+// 	document.location.href="portfolio/1.html"
+// 	// alert("true");
+// });
 $('.btn-social').mouseenter(function(){
 	$(this).addClass("btn-primary");
 	$(this).removeClass("btn-default");
@@ -12,16 +12,32 @@ $('.btn-social').mouseenter(function(){
 $('.btn-social').mouseleave(function(){
 	$(this).removeClass("btn-primary");
 	$(this).addClass("btn-default");
-})
-$('#btn-linkedin').click(function (){
-	document.location.href="https://ua.linkedin.com/in/bpalij"
-})
-$('#btn-github').click(function (){
-	document.location.href="https://github.com/bpalij"
-})
-$('#btn-codepen').click(function (){
-	document.location.href="https://codepen.io/bpalij/"
-})
+});
+// $('#btn-linkedin').click(function (){
+// 	document.location.href="https://ua.linkedin.com/in/bpalij"
+// });
+// $('#btn-github').click(function (){
+// 	document.location.href="https://github.com/bpalij"
+// })
+// $('#btn-codepen').click(function (){
+// 	document.location.href="https://codepen.io/bpalij/"
+// });
 $('#btn-fcc').click(function (){
 	document.location.href="https://www.freecodecamp.com/bpalij"
-})
+});
+function p1() {
+	document.location.href="portfolio/1.html"
+	// alert("true");
+};
+function clickOnGithub(){
+	document.location.href="https://github.com/bpalij"
+};
+function clickOnLinkedin(){
+	document.location.href="https://ua.linkedin.com/in/bpalij"
+};
+function clickOnCodepen(){
+	document.location.href="https://codepen.io/bpalij/"
+};
+function clickOnFcc(){
+	document.location.href="https://www.freecodecamp.com/bpalij"
+};
